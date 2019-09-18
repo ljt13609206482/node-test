75e276ac9ca5e3fcf0063b6c2c01100a30dc7509
lijiantao <lijiantao@zhiyuanhui.com>
2019-09-18 14:08:07 +0800

    搭建https服务器

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
   
 const Koa = require('koa');
-
+const logger = require('koa-logger');
+const sslify = require('koa-sslify').default;//http强制HTTPS
+const https = require('https');//node内置https server
+const fs = require('fs');
 const bodyParser = require('koa-bodyparser');
 
 const controller = require('./controller');
 
 const app = new Koa();
 
+app.use(logger())
+app.use(sslify())
 // log request URL:
 app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
     await next();
 });
+
+var options = {
+    key: fs.readFileSync('./private_key.pem'),  //私钥文件路径
+    cert: fs.readFileSync('./file.crt')  //证书文件路径
+};
 
 // parse request body:
 app.use(bodyParser());
@@ -19,6 +29,7 @@ app.use(bodyParser());
 // add controllers:
 app.use(controller());
 // 在端口8081监听:
-app.listen(8081);
-console.log('app started at port 8081...');
+https.createServer(options, app.callback()).listen(8081, () => {
+    console.log(`server running success at 8081`)
+});
 
